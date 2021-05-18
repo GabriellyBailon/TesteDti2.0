fa9df90ba6836f322cf096bf9fa3f8a4263a8c56
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-05-18 09:23:09 -0300

    Commit inicial com o código até a função de desempate

---
+++ TestePratico/Petshop.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestePratico
+{
+    class Petshop
+    {
+        string Nome;
+        double Distancia;
+        double TaxaBanhoDiaDeSemana_P;
+        double TaxaBanhoDiaDeSemana_G;
+        double TaxaBanhoFimDeSemana_P;
+        double TaxaBanhoFimDeSemana_G;
+
+        //Construtor
+        public Petshop(string nome, double distancia, 
+            double taxaBanhoDiaDeSemana_P, double taxaBanhoDiaDeSemana_G,
+            double taxaBanhoFimDeSemana_P, double taxaBanhoFimDeSemana_G)
+        {
+            Nome = nome;
+            Distancia = distancia;
+            TaxaBanhoDiaDeSemana_P = taxaBanhoDiaDeSemana_P;
+            TaxaBanhoDiaDeSemana_G = taxaBanhoDiaDeSemana_G;
+            TaxaBanhoFimDeSemana_P = taxaBanhoFimDeSemana_P;
+            TaxaBanhoFimDeSemana_G = taxaBanhoFimDeSemana_G;
+        }
+
+        public double CustoTotal(int diaDaSemana, int quantidadeCaesP, int quantidadeCaesG)
+        {
+            double custoTotal;
+            //Cálculo feito para pesquisa de segunda a sexta
+            if (diaDaSemana > 0 && diaDaSemana < 6)
+            {
+                custoTotal = (this.TaxaBanhoDiaDeSemana_P * quantidadeCaesP) + (this.TaxaBanhoDiaDeSemana_G * quantidadeCaesG);
+            }
+            else
+            {
+                custoTotal = (this.TaxaBanhoFimDeSemana_P * quantidadeCaesP) + (this.TaxaBanhoFimDeSemana_G * quantidadeCaesG);
+            }
+
+            return custoTotal;
+        }
+
+        public static double MelhorPreco(double custoMeuCaninoFeliz, double custoVaiRex, double custoChowChawgas)
+        {
+            double melhorPreco;
+
+            if(custoMeuCaninoFeliz < custoVaiRex && custoMeuCaninoFeliz < custoChowChawgas)
+            {
+                melhorPreco = custoMeuCaninoFeliz;
+            }
+            else if (custoVaiRex < custoMeuCaninoFeliz && custoVaiRex < custoChowChawgas)
+            {
+                melhorPreco = custoVaiRex;
+                Console.WriteLine("Vai Rex");
+            }
+            else if (custoChowChawgas < custoMeuCaninoFeliz && custoChowChawgas < custoVaiRex)
+            {
+                melhorPreco = custoChowChawgas;
+                Console.WriteLine("ChowChawgas");
+            }
+            else
+            {
+                melhorPreco = Desempate(custoMeuCaninoFeliz, custoVaiRex, custoChowChawgas);
+            }
+
+            return melhorPreco;
+        }
+
+        private static double Desempate(double custoMeuCaninoFeliz, double custoVaiRex, double custoChowChawgas)
+        {
+            double melhorPreco = 0.0;
+
+            if (custoMeuCaninoFeliz == custoChowChawgas || custoVaiRex == custoChowChawgas)
+            {
+                melhorPreco = custoChowChawgas;
+            }
+            else if (custoMeuCaninoFeliz == custoVaiRex)
+            {
+                melhorPreco = custoVaiRex;
+            }
+
+            return melhorPreco;
+        }
+    }
+
+    
+
+
+}
---
+++ TestePratico/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+
+namespace TestePratico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] dadosData;
+            int dia, mes, ano;
+            int diaDeHoje;
+            int quantidadeP, quantidadeG;
+            double custoTotal1, custoTotal2, custoTotal3;
+            double melhorCusto;
+
+            Petshop meuCaninoFeliz = new Petshop("Meu Canino Feliz", 2000, 20, 40, 24, 48);
+            Petshop vaiRex = new Petshop("Vai Rex", 1700, 15, 50, 20, 55);
+            Petshop chowChawgas = new Petshop("ChowChawgas", 800, 30, 45, 30, 45);
+
+            Console.WriteLine("Bem-vindo (a) a nossa calculadora do melhor valor!");
+            Console.WriteLine("Buscando o melhor preço para cuidar do seu melhor amigo!");
+
+            Console.WriteLine("\nVamos lá!");
+            Console.Write("Insira a data de hoje no formato DD/MM/AAAA (com as barrinhas): ");
+            dadosData = Console.ReadLine().Split('/');
+            dia = int.Parse(dadosData[0]);
+            mes = int.Parse(dadosData[1]);
+            ano = int.Parse(dadosData[2]);
+            diaDeHoje = CalculaDia.DiaDaSemana(dia, mes, ano);
+
+            Console.Write("Qual a quantidade de cãezinhos pequenos? ");
+            quantidadeP = int.Parse(Console.ReadLine());
+
+            Console.Write("Qual a quantidade de cãezinhos grandes? ");
+            quantidadeG = int.Parse(Console.ReadLine());
+
+            custoTotal1 = meuCaninoFeliz.CustoTotal(diaDeHoje, quantidadeP, quantidadeG);
+            custoTotal2 = vaiRex.CustoTotal(diaDeHoje, quantidadeP, quantidadeG);
+            custoTotal3 = chowChawgas.CustoTotal(diaDeHoje, quantidadeP, quantidadeG);
+
+            melhorCusto = Petshop.MelhorPreco(custoTotal1, custoTotal2, custoTotal3);
+
+            Console.WriteLine($"O melhor valor foi encontrado na " +
+                $"{melhorCusto.ToString("F2", CultureInfo.InvariantCulture)} por R$");
+        }
+    }
+}
