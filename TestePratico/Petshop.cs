0e8bfbb5c2df2d8d5febf43b9ad521282119bb24
Gabrielly Bailon <gabriellybailontur@gmail.com>
2021-05-19 08:43:29 -0300

    Colocadas Properties

--- TestePratico/Petshop.cs
+++ TestePratico/Petshop.cs
@@ -6,12 +6,13 @@ namespace TestePratico
 {
     class Petshop
     {
-        string Nome;
-        double Distancia;
-        double TaxaBanhoDiaDeSemana_P;
-        double TaxaBanhoDiaDeSemana_G;
-        double TaxaBanhoFimDeSemana_P;
-        double TaxaBanhoFimDeSemana_G;
+        public string Nome { get; set; }
+        public double Distancia { get; private set; }
+        public double TaxaBanhoDiaDeSemana_P { get; set; }
+        public double TaxaBanhoDiaDeSemana_G { get; set; }
+        public double TaxaBanhoFimDeSemana_P { get; set; }
+        public double TaxaBanhoFimDeSemana_G { get; set; }
+
 
         //Construtor
         public Petshop(string nome, double distancia, 
@@ -25,6 +26,7 @@ namespace TestePratico
             TaxaBanhoFimDeSemana_P = taxaBanhoFimDeSemana_P;
             TaxaBanhoFimDeSemana_G = taxaBanhoFimDeSemana_G;
         }
+
 
         public double CustoTotal(int diaDaSemana, int quantidadeCaesP, int quantidadeCaesG)
         {
